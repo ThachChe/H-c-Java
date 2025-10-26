b10097b523b69533c99c914e3f28939754f8bb07
unknown <thachche75@ gmail.com>
2025-10-26 15:25:06 +0700

    duanjavafirst

---
+++ src/thachche/HelloWorld.java
@@ -0,0 +1,11 @@
+package thachche;
+
+public final class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+
+	}
+}
